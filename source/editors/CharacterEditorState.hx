56c34170ab600df150bd59184858f7006ff4bba3
TheKitBoi <84044666+TheKitBoi@users.noreply.github.com>
2022-11-21 22:18:18 +0000

    Update CharacterEditorState.hx

--- source/editors/CharacterEditorState.hx
+++ source/editors/CharacterEditorState.hx
@@ -3,7 +3,7 @@ package editors;
 #if desktop
 import Discord.DiscordClient;
 #end
-import animateatlas.AtlasFrameMaker;
+import flxanimate.FlxAnimate;
 import flixel.FlxG;
 import flixel.FlxObject;
 import flixel.FlxSprite;
@@ -833,7 +833,7 @@ class CharacterEditorState extends MusicBeatState
 		}
 		var anims:Array<AnimArray> = char.animationsArray.copy();
 		if(Paths.fileExists('images/' + char.imageFile + '/Animation.json', TEXT)) {
-			char.frames = AtlasFrameMaker.construct(char.imageFile);
+			char.frames = FlxAnimate(char.imageFile);
 		} else if(Paths.fileExists('images/' + char.imageFile + '.txt', TEXT)) {
 			char.frames = Paths.getPackerAtlas(char.imageFile);
 		} else {
